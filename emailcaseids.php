fd4ca784dfecedc36f66eeebeb19efa63e38257c
pipeline <pipeline@jmaconsulting.biz>
2023-11-08 15:34:56 -0700

    Civix Upgrade to 23.11.8

--- emailcaseids.php
+++ emailcaseids.php
@@ -13,15 +13,6 @@ function emailcaseids_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function emailcaseids_civicrm_xmlMenu(&$files) {
-  _emailcaseids_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -31,60 +22,12 @@ function emailcaseids_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function emailcaseids_civicrm_postInstall() {
-  _emailcaseids_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function emailcaseids_civicrm_uninstall() {
-  _emailcaseids_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function emailcaseids_civicrm_enable() {
   _emailcaseids_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function emailcaseids_civicrm_disable() {
-  _emailcaseids_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function emailcaseids_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _emailcaseids_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function emailcaseids_civicrm_managed(&$entities) {
-  _emailcaseids_civix_civicrm_managed($entities);
 }
 
 function emailcaseids_civicrm_alterMailParams(&$params) {
@@ -97,53 +40,6 @@ function emailcaseids_civicrm_alterMailParams(&$params) {
   }
 }
 
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function emailcaseids_civicrm_caseTypes(&$caseTypes) {
-  _emailcaseids_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function emailcaseids_civicrm_angularModules(&$angularModules) {
-  _emailcaseids_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function emailcaseids_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _emailcaseids_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
- */
-function emailcaseids_civicrm_entityTypes(&$entityTypes) {
-  _emailcaseids_civix_civicrm_entityTypes($entityTypes);
-}
-
 // --- Functions below this ship commented out. Uncomment as required. ---
 
 /**
@@ -151,9 +47,8 @@ function emailcaseids_civicrm_entityTypes(&$entityTypes) {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function emailcaseids_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
